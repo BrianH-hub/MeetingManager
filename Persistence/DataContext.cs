d7187d596ae655956002640a37bb52f8061348c3
neilquizon <neil.quizon@gmail.com>
2020-04-02 17:06:07 -0700

    #1 added DataContext and DefaultConnection

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
