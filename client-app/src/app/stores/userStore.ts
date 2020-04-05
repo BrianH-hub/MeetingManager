7504df6d0cdecff9ba3c0918426eb8a1c2bec59a
Brian <1brianh@gmail.com>
2020-04-05 12:23:32 -0700

    commonStore

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -40,45 +40,6 @@ export default class UserStore {
     }
   };
 
-  @action getUser = async () => {
-    try {
-      const user = await agent.User.current();
-      runInAction(() => {
-        this.user = user;
-      });
-
-
-export default class UserStore {
-    rootStore: RootStore;
-  constructor(rootStore: RootStore) {
-    this.rootStore = rootStore;
-  }
-  @computed get isLoggedIn() {
-    return !!this.user;
-  }
-  @observable user: IUser | null = null;
-  @action login = async (values: IUserFormValues) => {
-    try {
-      const user = await agent.User.login(values);
-      runInAction(() => {
-        this.user = user;
-      });
-
-    } catch (error) {
-      throw error;
-
-    }
-  };
-
- @action register = async (values: IUserFormValues) => {
-    try {
-      const user = await agent.User.register(values);
-
-
-    } catch (error) {
-      throw error;
-    }
-  }
 
   @action getUser = async () => {
     try {
