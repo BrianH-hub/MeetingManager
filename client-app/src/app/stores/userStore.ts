56ca18dd244592950400855bf5c27c34bf592a11
Brian <1brianh@gmail.com>
2020-04-04 18:24:46 -0700

    login action

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,15 +1,21 @@
-import {observable, computed, action} from "mobx";
-import {IUser} from "../models/user";
+import { observable, computed, action } from "mobx";
+import { IUser, IUserFormValues } from "../models/user";
+import agent from "../api/agent";
 
 export default class UserStore {
-    @observable user: IUser | null = null;
+  @observable user: IUser | null = null;
 
-    @computed get isLoggedIn() {return !!this.user}
+  @computed get isLoggedIn() {
+    return !!this.user;
+  }
 
-    @action login = async (values: IUserFormValues) => {
-        try {
-        } catch (error) {
-            console.log(error)
-        }
-        }
+  @action login = async (values: IUserFormValues) => {
+    try {
+        const user = await agent.User.login(values);
+        this.user = user;
+
+    } catch (error) {
+      console.log(error);
     }
+  };
+}
