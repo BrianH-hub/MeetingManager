979712ad37014edbc67794e52b7e968e307a2728
Brian <1brianh@gmail.com>
2020-04-04 20:25:03 -0700

    checked w master, add loginform, homeP, and stores

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,21 +1,56 @@
-import { observable, computed, action } from "mobx";
+import { observable, computed, action, runInAction } from "mobx";
 import { IUser, IUserFormValues } from "../models/user";
 import agent from "../api/agent";
+import { RootStore } from "./rootStore";
+
 
 export default class UserStore {
-  @observable user: IUser | null = null;
-
+    rootStore: RootStore;
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+  }
   @computed get isLoggedIn() {
     return !!this.user;
   }
-
+  @observable user: IUser | null = null;
   @action login = async (values: IUserFormValues) => {
     try {
-        const user = await agent.User.login(values);
+      const user = await agent.User.login(values);
+      runInAction(() => {
         this.user = user;
+      });
 
+
+    } catch (error) {
+      throw error;
+
+    }
+  };
+
+ @action register = async (values: IUserFormValues) => {
+    try {
+      const user = await agent.User.register(values);
+
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @action getUser = async () => {
+    try {
+      const user = await agent.User.current();
+      runInAction(() => {
+        this.user = user;
+      });
     } catch (error) {
       console.log(error);
     }
+  };
+
+  @action logout = () => {
+
+    this.user = null;
+
   };
 }
