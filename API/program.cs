6787d9382f1f447fb8f08ce1772f437e3fbb48dd
mingwang168 <mingwang0904@gmail.com>
2020-04-05 17:16:32 -0700

    sfasdf

--- API/program.cs
+++ API/program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +10,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
-
 namespace API
 {
     public class Program
@@ -18,27 +17,23 @@ namespace API
         public static void Main(string[] args)
         {
             var host = CreateWebHostBuilder(args).Build();
-
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                try{
+                try
+                {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
                     Seed.SeedData(context);
-                    
                 }
                 catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");
-
                 }
-
             }
             host.Run();
         }
-
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
