296ac74a7dc0a878fb8d426ce82749faec1f0810
Brian <1brianh@gmail.com>
2020-04-05 18:40:13 -0700

    setup const

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext, Fragment } from "react";
+import { Container, Segment, Header, Button, Image } from "semantic-ui-react";
 
-export const HomePage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const HomePage = () => {
+  const { user, isLoggedIn } = rootStore.userStore;
+  const { openModal } = rootStore.modalStore;
+  const rootStore = useContext(RootStoreContext);
+  return <div></div>;
+};
+export default HomePage;
