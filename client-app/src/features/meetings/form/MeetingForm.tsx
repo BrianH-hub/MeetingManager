7cb2ad81d5c8924a344b9d21d58d30e714387cd3
Brian <1brianh@gmail.com>
2020-04-05 18:35:43 -0700

    meeting

--- client-app/src/features/meetings/form/MeetingForm.tsx
+++ client-app/src/features/meetings/form/MeetingForm.tsx
@@ -93,8 +93,14 @@ const MeetingForm: React.FC<RouteComponentProps<DetailParams>> = ({
   //outdated
 
   cont handleFinalFormSubmit = (values: any) => {
-    const { date, time, ...activity } = values;
+    const { date, time, ...meeting } = values;
     const dateAndTime = combineDateAndTime(values.date, values.time);
+    meeting.date = dateAndTime;
+    if (!meeting.id) {
+      let newActivity = {
+        ...meeting,
+        id: uuid()
+      };
   }
 
 >>>>>>> updating handleFinalForm
